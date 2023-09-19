774349698864e9a220aa8d14a3114511a57f795f
GentleOstrich <273819111@qq.com>
2023-09-19 12:27:14 +0800

    2023年9月19日12点26分

--- Lexer.h
+++ Lexer.h
@@ -6,63 +6,24 @@
 #define SYSY_COMPILER_LEXER_H
 
 #include <string>
+#include "LexType.h"
 
 #define RESERVEWORDS_NUM 12
-#define SOURCE_LEN_MAX 1024
-#define TOKEN_LEN_MAX 10
 
 using namespace std;
-enum LexType {
-    IDENFR,
-    INTCON,
-    STRCON,
-    MAINTK,
-    CONSTTK,
-    INTTK,
-    BREAKTK,
-    CONTINUETK,
-    IFTK,
-    ELSETK,
-    NOT,
-    AND,
-    OR,
-    FORTK,
-    GETINTTK,
-    PRINTFTK,
-    RETURNTK,
-    PLUS,
-    MINU,
-    VOIDTK,
-    MULT,
-    DIV,
-    MOD,
-    LSS,
-    LEQ,
-    GRE,
-    EQL,
-    NEQ,
-    ASSIGN,
-    SEMICN,
-    COMMA,
-    LPARENT,
-    RPARENT,
-    LBRACK,
-    RBRACK,
-    LBRACE,
-    RBRACE
-};
 
 
 class Lexer {
 private:
-    string source[SOURCE_LEN_MAX];
+    string source;
     int curPos = 0;
-    string token[TOKEN_LEN_MAX];
+    string token;
     string reserveWords[RESERVEWORDS_NUM] = {
             "main", "const", "int", "break", "continue", "if", "else", "for", "getint", "printf", "return", "void"
     };
+    LexType lexType;
     int lineNum = 0;
-    int number = 0;
+    int number;
 
     int isReserveWord(const string &token) {
         for (int i = 0; i < RESERVEWORDS_NUM; ++i) {
@@ -75,20 +36,267 @@ private:
 
 public:
     int next() {
+        token.clear();
+        //处理完毕
+        if (curPos >= source.length()) {
+            return 1;
+        }
+        // 空格 制表符 回车
+        while (source[curPos] == '\t' || source[curPos] == ' ' || source[curPos] == '\n') {
+            if (source[curPos] == '\n') {
+                lineNum++;
+            }
+            curPos++;
+        }
 
+        // 标识符 保留字
+        if (isalpha(source[curPos]) || source[curPos] == '_') {
+            while (isalpha(source[curPos]) || isdigit(source[curPos]) || source[curPos] == '_') {
+                token += source[curPos];
+                curPos++;
+            }
+            int reserveValue = isReserveWord(token);
+            cout << reserveValue << endl;
+            switch (reserveValue) {
+                case 0:
+                    lexType = LexType::IDENFR;
+                case 1:
+                    lexType = LexType::MAINTK;
+                case 2:
+                    lexType = LexType::CONSTTK;
+                case 3:
+                    lexType = LexType::INTTK;
+                case 4:
+                    lexType = LexType::BREAKTK;
+                case 5:
+                    lexType = LexType::CONTINUETK;
+                case 6:
+                    lexType = LexType::IFTK;
+                case 7:
+                    lexType = LexType::ELSETK;
+                case 8:
+                    lexType = LexType::FORTK;
+                case 9:
+                    lexType = LexType::GETINTTK;
+                case 10:
+                    lexType = LexType::PRINTFTK;
+                case 11:
+                    lexType = LexType::RETURNTK;
+                case 12:
+                    lexType = LexType::VOIDTK;
+                default:
+                    lexType = LexType::ERROR;
+            }
+//            if (lexType == LexType::ERROR) {
+//                return -1;
+//            }
+            return 0;
+        }
+
+        // 无符号整数
+        if (isdigit(source[curPos])) {
+            while (isdigit(source[curPos])) {
+                token += source[curPos];
+                curPos++;
+            }
+            number = atoi(token.c_str());
+            lexType = LexType::INTCON;
+            return 0;
+        }
+
+        // 字符串
+        if (source[curPos] == '\"') {
+            token += source[curPos];
+            curPos++;
+            while (source[curPos] != '\"') {
+                token += source[curPos];
+                curPos++;
+            }
+            token += source[curPos];
+            curPos++;
+            lexType = LexType::STRCON;
+            return 0;
+        }
+
+        //  ! && || + - * / %  < <= > >= == != = ; , ( ) [ ] { }
+        if (source[curPos] == '!') {
+            token += '!';
+            lexType = LexType::NOT;
+            curPos++;
+            return 0;
+        }
+        if (source[curPos] == '&' && source[curPos + 1] == '&') {
+            token += "&&";
+            lexType = LexType::AND;
+            curPos += 2;
+            return 0;
+        }
+        if (source[curPos] == '|' && source[curPos + 1] == '|') {
+            token += "||";
+            lexType = LexType::OR;
+            curPos += 2;
+            return 0;
+        }
+        if (source[curPos] == '+') {
+            token += '+';
+            lexType = LexType::PLUS;
+            curPos++;
+            return 0;
+        }
+        if (source[curPos] == '-') {
+            token += '-';
+            lexType = LexType::MINU;
+            curPos++;
+            return 0;
+        }
+        if (source[curPos] == '*') {
+            token += '*';
+            lexType = LexType::MULT;
+            curPos++;
+            return 0;
+        }
+        if (source[curPos] == '%') {
+            token += '%';
+            lexType = LexType::MOD;
+            curPos++;
+            return 0;
+        }
+        if (source[curPos] == '<' && source[curPos + 1] == '=') {
+            token += "<=";
+            lexType = LexType::LEQ;
+            curPos += 2;
+            return 0;
+        }
+        if (source[curPos] == '<') {
+            token += '<';
+            lexType = LexType::LSS;
+            curPos++;
+            return 0;
+        }
+        if (source[curPos] == '>' && source[curPos + 1] == '=') {
+            token += ">=";
+            lexType = LexType::GEQ;
+            curPos += 2;
+            return 0;
+        }
+        if (source[curPos] == '>') {
+            token += '>';
+            lexType = LexType::GRE;
+            curPos += 1;
+            return 0;
+        }
+        if (source[curPos] == '=' && source[curPos + 1] == '=') {
+            token += "==";
+            lexType = LexType::EQL;
+            curPos += 2;
+            return 0;
+        }
+        if (source[curPos] == '!' && source[curPos + 1] == '=') {
+            token += "==";
+            lexType = LexType::NEQ;
+            curPos += 2;
+            return 0;
+        }
+        if (source[curPos] == '=') {
+            token += '=';
+            lexType = LexType::ASSIGN;
+            curPos++;
+            return 0;
+        }
+        if (source[curPos] == ';') {
+            token += ';';
+            lexType = LexType::SEMICN;
+            curPos++;
+            return 0;
+        }
+        if (source[curPos] == ',') {
+            token += ',';
+            lexType = LexType::COMMA;
+            curPos++;
+            return 0;
+        }
+        if (source[curPos] == '(') {
+            token += '(';
+            lexType = LexType::LPARENT;
+            curPos++;
+            return 0;
+        }
+        if (source[curPos] == ')') {
+            token += ')';
+            lexType = LexType::RPARENT;
+            curPos++;
+            return 0;
+        }
+        if (source[curPos] == '[') {
+            token += '[';
+            lexType = LexType::LBRACK;
+            curPos++;
+            return 0;
+        }
+        if (source[curPos] == ']') {
+            token += ']';
+            lexType = LexType::RBRACK;
+            curPos++;
+            return 0;
+        }
+        if (source[curPos] == '{') {
+            token += '{';
+            lexType = LexType::LBRACE;
+            curPos++;
+            return 0;
+        }
+        if (source[curPos] == '}') {
+            token += '}';
+            lexType = LexType::RBRACE;
+            curPos++;
+            return 0;
+        }
+
+        // 注释
+        if (source[curPos] == '/') {
+            if (curPos < source.length() && source[curPos + 1] == '/') {
+                curPos += 2;
+                while (curPos < source.length() && source[curPos] != '\n') {
+                    curPos++;
+                }
+                return next();
+            } else if (curPos < source.length() && source[curPos + 1] == '*') {
+                curPos += 2;
+                while (curPos < source.length()) {
+                    while (curPos < source.length() && source[curPos] != '*') {
+                        if (source[curPos] == '\n') {
+                            lineNum++;
+                        }
+                        curPos++;
+                    }
+                    while (curPos < source.length() && source[curPos] == '*') {
+                        curPos++;
+                    }
+                    if (curPos < source.length() && source[curPos] == '/') {
+                        curPos++;
+                        break;
+                    }
+                }
+                return next();
+            } else {
+                lexType = LexType::DIV;
+                curPos++;
+            }
+        }
+        return 0;
     }
 
     string getToken() {
-        if (isReserveWord("hello") == 0) {
-
-        }
+        return token;
     }
 
     LexType getLexType() {
-
+        return lexType;
     }
 
-
+    void setSource(string source) {
+        this->source = source;
+    }
 };
 
 
--- Scanner.cpp
+++ Scanner.cpp
@@ -1,12 +1,117 @@
 //
 // Created by yh on 2023/9/18.
 //
-
+#include <iostream>
+#include <fstream>
 #include "Lexer.h"
 
+using namespace std;
+#define FILEPATH "D:\\University\\Study\\2023fall\\compiler\\SysY-Compiler\\testfile.txt"
+
+string LexType2String(LexType lexType) {
+    switch (lexType) {
+        case LexType::IDENFR:
+            return "IDENFR";
+        case LexType::MAINTK:
+            return "MAINTK";
+        case LexType::CONSTTK:
+            return "CONSTTK";
+        case LexType::INTTK:
+            return "INTTK";
+        case LexType::BREAKTK:
+            return "BREAKTK";
+        case LexType::CONTINUETK:
+            return "CONTINUETK";
+        case LexType::IFTK:
+            return "IFTK";
+        case LexType::ELSETK:
+            return "ELSETK";
+        case LexType::FORTK:
+            return "FORTK";
+        case LexType::GETINTTK:
+            return "GETINTTK";
+        case LexType::PRINTFTK:
+            return "PRINTFTK";
+        case LexType::RETURNTK:
+            return "RETURNTK";
+        case LexType::VOIDTK:
+            return "VOIDTK";
+        case LexType::INTCON:
+            return "INTCON";
+        case LexType::STRCON:
+            return "STRCON";
+        case LexType::NOT:
+            return "NOT";
+        case LexType::AND:
+            return "AND";
+        case LexType::OR:
+            return "OR";
+        case LexType::PLUS:
+            return "PLUS";
+        case LexType::MINU:
+            return "MINU";
+        case LexType::MULT:
+            return "MULT";
+        case LexType::DIV:
+            return "DIV";
+        case LexType::MOD:
+            return "MOD";
+        case LexType::LSS:
+            return "LSS";
+        case LexType::LEQ:
+            return "LEQ";
+        case LexType::GRE:
+            return "GRE";
+        case LexType::GEQ:
+            return "GEQ";
+        case LexType::EQL:
+            return "EQL";
+        case LexType::NEQ:
+            return "NEQ";
+        case LexType::ASSIGN:
+            return "ASSIGN";
+        case LexType::SEMICN:
+            return "SEMICN";
+        case LexType::COMMA:
+            return "COMMA";
+        case LexType::LPARENT:
+            return "LPARENT";
+        case LexType::RPARENT:
+            return "RPARENT";
+        case LexType::LBRACK:
+            return "LBRACK";
+        case LexType::RBRACK:
+            return "RBRACK";
+        case LexType::LBRACE:
+            return "LBRACE";
+        case LexType::RBRACE:
+            return "RBRACE";
+        case LexType::ERROR:
+            return "ERROR";
+        default:
+            return "ERROR";
+    }
+}
 
 int main() {
     Lexer lexer;
+    ifstream ifs(FILEPATH);
+    ofstream ofs("output.txt");
+    string source;
+    if (ifs.is_open() && ofs.is_open()) {
+        source = string((istreambuf_iterator<char>(ifs)), istreambuf_iterator<char>());
+        lexer.setSource(source);
+        while (1) {
+            bool isEnd = lexer.next();
+            if (isEnd) {
+                break;
+            }
+            string token = lexer.getToken();
+            LexType lexType = lexer.getLexType();
+            cout << LexType2String(lexType) << " " << token << endl;
+        }
+    }
+
 
     return 0;
 }
