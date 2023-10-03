f9c25e186bbb3fe785382576a308a6a19e88a4a5
GentleOstrich <273819111@qq.com>
2023-10-03 15:28:39 +0800

    socre 100

---
+++ cmake-build-release/CMakeFiles/SysY-Compiler.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SysY-Compiler.dir/Lexer/Lexer.cpp.obj"
+  "CMakeFiles/SysY-Compiler.dir/Parser/Parser.cpp.obj"
+  "CMakeFiles/SysY-Compiler.dir/main.cpp.obj"
+  "SysY-Compiler.exe"
+  "SysY-Compiler.exe.manifest"
+  "SysY-Compiler.lib"
+  "SysY-Compiler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SysY-Compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
