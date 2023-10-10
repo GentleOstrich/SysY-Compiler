e36d8646ab39b11bbcea50d3f88e3319fbb6b7f4
GentleOstrich <21373302@buaa.edu.cn>
2023-10-10 20:05:20 +0800

    temp

---
+++ cmake-build-debug/CMakeFiles/SysY-Compiler.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yh/SysY-Compiler/Lexer/Lexer.cpp" "/home/yh/SysY-Compiler/cmake-build-debug/CMakeFiles/SysY-Compiler.dir/Lexer/Lexer.cpp.o"
+  "/home/yh/SysY-Compiler/Parser/Parser.cpp" "/home/yh/SysY-Compiler/cmake-build-debug/CMakeFiles/SysY-Compiler.dir/Parser/Parser.cpp.o"
+  "/home/yh/SysY-Compiler/main.cpp" "/home/yh/SysY-Compiler/cmake-build-debug/CMakeFiles/SysY-Compiler.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/SysY-Compiler.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SysY-Compiler.dir/Lexer/Lexer.cpp.o"
+  "CMakeFiles/SysY-Compiler.dir/Parser/Parser.cpp.o"
+  "CMakeFiles/SysY-Compiler.dir/main.cpp.o"
+  "SysY-Compiler"
+  "SysY-Compiler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SysY-Compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
