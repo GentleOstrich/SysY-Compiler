d3230ee33aea7ff9d074b64e0d31d4d25f5b7023
GentleOstrich <273819111@qq.com>
2023-10-20 12:56:38 +0800

    temp

---
+++ cmake-build-debug/CMakeFiles/SysY-Compiler.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SysY-Compiler.dir/ErrorCheck/ErrorCheck.cpp.obj"
+  "CMakeFiles/SysY-Compiler.dir/Lexer/Lexer.cpp.obj"
+  "CMakeFiles/SysY-Compiler.dir/Parser/Parser.cpp.obj"
+  "CMakeFiles/SysY-Compiler.dir/SymbolManager/Func.cpp.obj"
+  "CMakeFiles/SysY-Compiler.dir/SymbolManager/Symbol.cpp.obj"
+  "CMakeFiles/SysY-Compiler.dir/main.cpp.obj"
+  "SysY-Compiler.exe"
+  "SysY-Compiler.exe.manifest"
+  "SysY-Compiler.pdb"
+  "libSysY-Compiler.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SysY-Compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-release/CMakeFiles/SysY-Compiler.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SysY-Compiler.
+# This may be replaced when dependencies are built.
